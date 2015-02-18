e14c2d9a0dcd5fdb3fa1a3fa9d85ce676f05bbe4
Maxime Poulin <mpoulin@roux.ca>
2015-02-17 22:38:20 -0500

    Display of device name and given id

--- app.js
+++ app.js
@@ -10,7 +10,14 @@ client({ path: 'http://10.0.50.10:8083/ZWaveAPI/Data/0' }).then(function(res) {
   var response = res.entity;
   var allDevices = res.entity;
 
+  var controllerId = allDevices.controller.data.nodeId.value;
+
   _.forEach(allDevices.devices, function(device, deviceId) {
+    if (deviceId == 255 || deviceId == controllerId || device.data.isVirtual.value) {
+      return;
+    }
+    console.log("### " + device.data.givenName.value + " (" + deviceId + ") ###");
+
     _.forEach(device.instances, function(instance, instanceId) {
       var sensorBinary = instance.commandClasses[0x30];
       var sensorMultilevel = instance.commandClasses[0x31];
